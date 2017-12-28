1c3347d3c0fdf0a75900e15c054e4f4ed8040fc1
Manoj Jeswani <manojjeswanig@gmail.com>
2017-12-28 16:04:39 +0530

    face-eye-detection-added

---
+++ live-cam-face-eyes-detect-haar-cascades-ipwebcam.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import urllib.request
+
+face_classifier = cv2.CascadeClassifier('Haarcascades/haarcascade_frontalface_default.xml')
+eye_classifier = cv2.CascadeClassifier('Haarcascades/haarcascade_eye.xml')
+
+def face_detector(img, size=0.5):
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    if faces is ():
+        return img
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),5)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_classifier.detectMultiScale(roi_gray,1.3,5)
+        
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),5) 
+            
+    return img
+
+
+
+stream = urllib.request.urlopen('http://192.168.43.1:8080/video') #here use your server ip/video
+bytes = bytes()
+
+
+while True:
+    bytes += stream.read(1024)
+    a = bytes.find(b'\xff\xd8')
+    b = bytes.find(b'\xff\xd9')
+    if a != -1 and b != -1:
+        jpg = bytes[a:b+2]
+        bytes = bytes[b+2:]
+        frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+        cv2.namedWindow('Face Extractor',cv2.WINDOW_NORMAL)
+
+        cv2.imshow('Face Extractor', face_detector(frame))
+    if cv2.waitKey(1) == 13: #13 is the Enter Key
+        break
+        
+stream.release()
+cv2.destroyAllWindows()
